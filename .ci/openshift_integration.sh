eb2c2f0d687e5d06a8a07b554ae944de0fee3af6
Kim Tsao <84398375+kim-tsao@users.noreply.github.com>
2022-09-23 11:19:42 -0400

    remove incorrect licenses (#26)
    Signed-off-by: Kim Tsao <ktsao@redhat.com>
    
    Signed-off-by: Kim Tsao <ktsao@redhat.com>

--- .ci/openshift_integration.sh
+++ .ci/openshift_integration.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-#
-# Copyright (c) 2020 Red Hat, Inc.
-# This program and the accompanying materials are made
-# available under the terms of the Eclipse Public License 2.0
-# which is available at https://www.eclipse.org/legal/epl-2.0/
-#
-# SPDX-License-Identifier: EPL-2.0
-#
 
 #!/usr/bin/env bash
 # exit immediately when a command fails
