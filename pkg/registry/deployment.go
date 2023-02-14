3fabf1bc2e8cda083b4681fce5189bde6eacbac2
Michael Valdron <mvaldron@redhat.com>
2023-02-14 15:10:48 -0500

    add missing env vars to registry viewer deployment spec.
    Signed-off-by: Michael Valdron <mvaldron@redhat.com>

--- pkg/registry/deployment.go
+++ pkg/registry/deployment.go
@@ -17,6 +17,8 @@ limitations under the License.
 package registry
 
 import (
+	"fmt"
+
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
@@ -229,6 +231,23 @@ func GenerateDeployment(cr *registryv1alpha1.DevfileRegistry, scheme *runtime.Sc
 				Limits: corev1.ResourceList{
 					corev1.ResourceCPU:    resource.MustParse("500m"),
 					corev1.ResourceMemory: resource.MustParse("256Mi"),
+				},
+			},
+			Env: []corev1.EnvVar{
+				{
+					Name:  "ANALYTICS_WRITE_KEY",
+					Value: cr.Spec.Telemetry.RegistryViewerWriteKey,
+				},
+				{
+					Name: "DEVFILE_REGISTRIES",
+					Value: fmt.Sprintf(`
+					[
+						{
+							"name": "Community",
+							"url": "http://localhost:8080",
+							"fqdn": "%s"
+						}
+					]`, cr.Status.URL),
 				},
 			},
 			VolumeMounts: []corev1.VolumeMount{
