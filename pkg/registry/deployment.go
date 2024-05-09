cf288334ba553638e982b5c9d6d71dd2ab4b4f51
Jordan Dubrick <jdubrick@redhat.com>
2024-05-09 16:18:34 -0400

    prefix http protocol to url
    Signed-off-by: Jordan Dubrick <jdubrick@redhat.com>

--- pkg/registry/deployment.go
+++ pkg/registry/deployment.go
@@ -274,7 +274,7 @@ func GenerateDeployment(cr *registryv1alpha1.DevfileRegistry, scheme *runtime.Sc
 					[
 						{
 							"name": "%s",
-							"url": "%s",
+							"url": "http://%s",
 							"fqdn": "%s"
 						}
 					]`, cr.ObjectMeta.Name, localHostname, cr.Status.URL),
